d3910cae589218c6697949ea9c49232b82ab9f7f
Tom Burback <burbacktg@ad.msoe.edu>
2019-08-26 02:03:53 -0500

    Updated serial code. Still needs a lot of work!

--- serial_grab/serial_grab.ino
+++ serial_grab/serial_grab.ino
@@ -1,24 +1,27 @@
-const int led=8;
-int value=0;
+#include <string.h>
 
-void setup() 
-   { 
-      Serial.begin(9600); 
-      pinMode(led, OUTPUT);
-      digitalWrite (led, LOW);
-      Serial.println("Connection established...");
-   }
- 
-void loop() 
-   {
-     while (Serial.available())
-        {
-           value = Serial.read();
-        }
-     
-     if (value == '1')
-        digitalWrite (led, HIGH);
-     
-     else if (value == '0')
-        digitalWrite (led, LOW);
-   }
+void setup() {
+  //Open serial port with 9600 baud
+  Serial.begin(9600);
+  //set all digital pins to output
+  for(int i = 0; i <= 13; i++) {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, LOW);
+  }
+  Serial.println("Setup completed!\n");
+}
+
+void loop() {
+  int serial_in = 0;
+  while(Serial.available()) {
+    serial_in = Serial.read();
+  }
+  switch(serial_in) {
+    case '0':
+      digitalWrite(8, LOW);
+      break;
+    case '1':
+      digitalWrite(8, HIGH);
+      break;
+  }
+}
